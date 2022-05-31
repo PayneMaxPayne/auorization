65b2d1004433f9623bbcda1dd341ee3f34f1a5a5
PayneMaxPayne <75696288+PayneMaxPayne@users.noreply.github.com>
2022-05-31 18:34:50 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function showPassword() {
+  const btn = document.querySelector(".password_btn");
+  const input = document.querySelector(".password__input");
+
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+    if (input.getAttribute("type") === "password") {
+      input.setAttribute("type", "text");
+    } else {
+      input.setAttribute("type", "password");
+    }
+  });
+}
+showPassword();
